9b4a073681e2d761d25793c219f1798c0383a878
moath othman <dark2torch@gmail.com>
2020-05-07 03:25:50 +0300

    make it better

--- models.py
+++ models.py
@@ -1,10 +1,28 @@
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, create_engine, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
+import os
+ 
 
 database_path = os.environ['DATABASE_URL']
 
-db = SQLAlchemy()
+db = SQLAlchemy( )
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename
+    variable to have multiple verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    print("lets drop and create the db", db)
+    db.drop_all()
+    db.create_all()
+
+# db_drop_and_create_all()
 
 '''
 setup_db(app)
