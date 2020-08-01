d7887c7c6dda41c95ff91359016d5a762e1a5f4b
moath othman <dark2torch@gmail.com>
2020-08-01 19:15:32 +0300

    script update:

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export AUTH0_DOMAIN="moathothman.auth0.com"
+export ALGORITHMS=['RS256'] 
+export API_AUDIENCE='capstone'
+export CLIENT_ID='OJexo9qKwUpl0eVyD9HIZklsxBKw7L3X'
+export DATABASE_URL='postgres://localhost:5432/capstone'
+export EXCITED=true
